bfa57d9d83853c078b1a62a7696afcbd2c68502b
@amritmohan <amritmohan2284@gmail.com>
2025-02-14 00:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var selectElement = document.getElementById("colorSelect");
+        var buttonElement = document.querySelector("input[type='button']");
+
+        buttonElement.addEventListener("click", () => {
+            // Get the selected option
+            var selectedIndex = selectElement.selectedIndex; // Get the selected option index
+            if (selectedIndex !== -1) { // Ensure an option is selected
+                selectElement.remove(selectedIndex); // Remove the selected option
+            }
+
+        })
